b01bdf7f1e4a6d8eb5309f5f6e36461df8c6a247
stacycaeiro <stacycaeiro2025@u.northwestern.edu>
2024-03-02 12:00:21 -0600

    outline final report, exploration of target var

--- r_scripts/01_data_exploration.R
+++ r_scripts/01_data_exploration.R
@@ -1,24 +1,33 @@
 library(tidyverse)
-library(naniar)
+library(skimr)
 
+# skim data ----
 articles <- read_csv("data/OnlineNewsPopularity.csv")
 
 glimpse(articles)
 
-news_articles |>
-  select(is.numeric)
+skim(articles)
 
-gg_miss_var(articles)
+gg_miss_var(articles) # no missing variables
 
-ggplot(articles, aes(x = shares)) +
+# plot target variable ----
+
+shares_hist <- ggplot(articles, aes(x = shares)) +
   geom_histogram() + 
   xlim(0, 75000) + 
-  ylim(0, 17500)
+  ylim(0, 17500) +
+  labs(title = "Frequency of Social Media Shares",
+       subtitle = "Most articles receive <10,000 shares",
+       x = "Shares",
+       y = "Count")
+
+ggsave(filename = "img/shares_hist.png")
 
 ggplot(articles, aes(x = log(shares))) + 
   geom_histogram(bins = 30) 
 
 articles |>
   select(shares) |>
-  summarize(max = max(shares, na.rm = TRUE))
+  summarize(max = max(shares, na.rm = TRUE),
+            min = min(shares, na.rm = TRUE))
 
