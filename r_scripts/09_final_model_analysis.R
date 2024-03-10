ea70015881908fc0f1270ecce92c6c656846727c
stacycaeiro <stacycaeiro2025@u.northwestern.edu>
2024-03-10 13:19:41 -0500

    final analysis log and original scale

--- r_scripts/09_final_model_analysis.R
+++ r_scripts/09_final_model_analysis.R
@@ -21,14 +21,68 @@ num_cores <- parallel::detectCores(logical = TRUE)
 doMC::registerDoMC(cores = num_cores)
 
 # metrics on shares log ----
-rf_metrics <- metric_set(rmse, rsq, mae)
+final_metrics <- metric_set(rmse, rsq, mae)
 
 rf_predict <- articles_test |> 
   bind_cols(predict(rf_fit_final, articles_test)) |> 
   select(shares_log, .pred)
 
-rf_metrics(rf_predict, 
+rf_final_metrics <- final_metrics(rf_predict, 
            truth = shares_log, 
            estimate = .pred)
 
-save(rf_metrics, file = here("results/rf_final_metrics.rda"))
+within_10_pct <- rf_predict |>
+  summarize(
+    within_10_pct = mean(
+      (abs(.pred - shares_log)/shares_log )
+      <= 0.10)
+  )
+
+within_10_pct
+
+save(rf_final_metrics, within_10_pct,
+     file = here("results/rf_final_metrics.rda"))
+
+ggplot(rf_predict, aes(x = shares_log, y = .pred)) +
+  geom_point(alpha = 0.5) +
+  geom_abline() +
+  labs(title = "Predicted Shares vs Actual Shares on Log Scale",
+       x = "Actual Shares",
+       y = "Predicted Shares") 
+
+ggsave(filename = "img/pred_vs_shares_log.png")
+
+# metrics on original scale ---- 
+rf_predict_scaled <- articles_test |>
+  bind_cols(predict(rf_fit_final, articles_test)) |>
+  mutate(.pred_scaled = exp(.pred)) |>
+  select(shares, .pred_scaled)
+
+rf_predict_scaled
+
+rf_final_metrics_scaled <- final_metrics(rf_predict_scaled, 
+                               truth = shares, 
+                               estimate = .pred_scaled)
+
+rf_final_metrics_scaled
+
+within_10_pct_scaled <- rf_predict_scaled |>
+  summarize(
+    within_10_pct = mean(
+      (abs(.pred_scaled - shares)/shares )
+      <= 0.10)
+  )
+
+within_10_pct_scaled
+
+save(rf_final_metrics_scaled, within_10_pct_scaled, 
+     file = here("results/rf_final_metrics_scaled.rda"))
+
+ggplot(rf_predict_scaled, aes(x = shares, y = .pred_scaled)) +
+  geom_point(alpha = 0.5) +
+  geom_abline() +
+  labs(title = "Predicted Shares vs Actual Shares on Original Scale",
+       x = "Actual Shares",
+       y = "Predicted Shares") 
+
+ggsave(filename = "img/pred_vs_shares_scaled.png")
