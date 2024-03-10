48fc6c203e8a905b197f5edd068c22e5846a037b
stacycaeiro <stacycaeiro2025@u.northwestern.edu>
2024-03-10 11:52:03 -0500

    table of rmse values

--- r_scripts/07_model_analysis_new.R
+++ r_scripts/07_model_analysis_new.R
@@ -40,7 +40,63 @@ null_basic_rmse <- show_best(null_fit, metric = "rmse") |>
   slice_head(n = 1) |>
   mutate(Model = "Null (Basic)")
 
-lm_basic_rmse <- show_best(lm_, metric = "rmse") |>
+lm_basic_rmse <- show_best(lm_fit, metric = "rmse") |>
   slice_head(n = 1) |>
-  mutate(Model = "Null (Basic)")
+  mutate(Model = "Linear (Basic)")
 
+rf_basic_rmse <- show_best(tuned_rf_basic, metric = "rmse") |>
+  slice_head(n = 1) |>
+  mutate(Model = "Random Forest (Basic)")
+
+knn_basic_rmse <- show_best(tuned_knn_basic, metric = "rmse") |>
+  slice_head(n = 1) |>
+  mutate(Model = "K-Nearest Neighbors (Basic)")
+
+en_basic_rmse <- show_best(tuned_en_basic, metric = "rmse") |>
+  slice_head(n = 1) |>
+  mutate(Model = "Elastic Net (Basic)")
+
+bt_rmse <- show_best(tuned_bt_fe, metric = "rmse") |>
+  slice_head(n = 1) |>
+  mutate(Model = "Boosted Tree")
+
+null_rmse <- show_best(null_fit_fe, metric = "rmse") |>
+  slice_head(n = 1) |>
+  mutate(Model = "Null")
+
+lm_rmse <- show_best(lm_fit_fe, metric = "rmse") |>
+  slice_head(n = 1) |>
+  mutate(Model = "Linear")
+
+rf_rmse <- show_best(tuned_rf_fe, metric = "rmse") |>
+  slice_head(n = 1) |>
+  mutate(Model = "Random Forest")
+
+knn_rmse <- show_best(tuned_knn_fe, metric = "rmse") |>
+  slice_head(n = 1) |>
+  mutate(Model = "K-Nearest Neighbors")
+
+en_rmse <- show_best(tuned_en_fe, metric = "rmse") |>
+  slice_head(n = 1) |>
+  mutate(Model = "Elastic Net")
+
+# rmse tibble
+rmse_tibble <- bind_rows(
+  null_basic_rmse, 
+  null_rmse,
+  lm_basic_rmse,
+  lm_rmse,
+  en_basic_rmse,
+  en_rmse,
+  rf_basic_rmse,
+  rf_rmse,
+  en_basic_rmse,
+  en_rmse,
+  knn_basic_rmse,
+  knn_rmse,
+  bt_basic_rmse,
+  bt_rmse
+) |>
+  arrange(mean)
+
+save(rmse_tibble, file = here("results/rmse_tibble.rda"))
